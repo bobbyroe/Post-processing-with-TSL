be52b1905d6bdfacc355c290f2f91daf42aa69c2
Bobby Roe <bobby.roe@unifyconsulting.com>
2025-10-04 10:54:10 -0700

    add more anims

--- index.js
+++ index.js
@@ -15,14 +15,13 @@ import { outline } from 'three/addons/tsl/display/OutlineNode.js';
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
+
 scene.background = new THREE.Color(0x000000);
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 4;
 const renderer = new THREE.WebGPURenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.outputColorSpace = THREE.SRGBColorSpace;
 
@@ -38,7 +37,6 @@ controls.dampingFactor = 0.05;
 
 const manager = new THREE.LoadingManager();
 const loader = new FBXLoader(manager);
-const textureLoader = new THREE.TextureLoader();
 const path = "./assets/Y Bot.fbx";
 let character;
 const sceneData = {
@@ -48,8 +46,8 @@ const sceneData = {
 loader.load(path, (fbx) => {
 
   function getMaterial() {
-    const material = new THREE.MeshMatcapMaterial({
-      matcap: textureLoader.load("./assets/fire-edge-blue.jpg"),
+    const material = new THREE.MeshLambertMaterial({
+      color: 0x000000,
     });
     return material;
   }
@@ -57,14 +55,13 @@ loader.load(path, (fbx) => {
   function initCharacter(fbx) {
     const char = fbx;
     char.scale.setScalar(0.02);
-    char.position.set(0, -1.5, 0);
+    char.position.set(0, -2, 0);
     char.traverse((c) => {
       if (c.isMesh) {
         if (c.material.name === "Alpha_Body_MAT") {
-          // c.material.color = new THREE.Color(0x994400);
-          // c.material = getMaterial();
+          c.material.color = new THREE.Color(0xbbbb00);
         }
-        c.castShadow = true;
+        // c.material = getMaterial();
       }
     });
 
@@ -81,24 +78,28 @@ loader.load(path, (fbx) => {
 
 //
 const animations = [
-  "Being Electrocuted",
-  "Drunk Walk",
+  // "Being Electrocuted",
+  // "Drunk Walk",
+  "Female Dynamic Pose",
+  "Female Dynamic Pose-02",
   "Floating",
-  "Idle",
-  "Joyful Jump",
-  "Kneeling Pointing",
-  "Low Crawl",
-  "Male Dance Pose",
-  "Neutral Idle",
-  "Reaction",
-  "Spat In Face",
-  "Stand To Roll",
+  "Flying",
+  // "Idle",
+  // "Joyful Jump",
+  // "Kneeling Pointing",
+  // "Low Crawl",
+  // "Male Dance Pose",
+  "Male Dynamic Pose",
+  // "Neutral Idle",
+  // "Reaction",
+  // "Spat In Face",
+  // "Stand To Roll",
   "Standard Walk",
   "Swimming",
-  "Thriller Part 3",
+  // "Thriller Part 3",
   "Treading Water",
   "Walking",
-  "Waving",
+  // "Waving",
 ];
 const apath = "./assets/animations/";
 manager.onLoad = () => initScene(sceneData);
@@ -123,17 +124,6 @@ function initScene(sceneData) {
   const { character, animations } = sceneData;
   const actions = setupActions(character, animations);
   scene.add(character);
-  // ground
-  const radius = 10;
-  const geometry = new THREE.CircleGeometry(radius, 32);
-  const material = new THREE.MeshStandardMaterial({
-    color: 0x001020,
-  });
-  const plane = new THREE.Mesh(geometry, material);
-  plane.rotation.x = Math.PI * -0.5;
-  plane.receiveShadow = true;
-  plane.position.y = -1.5;
-  scene.add(plane);
 
   const sunLight = new THREE.DirectionalLight(0xffffff, 5);
   sunLight.position.set(2, 4, 3);
@@ -144,17 +134,20 @@ function initScene(sceneData) {
   backLight.position.set(-2, 0, -3);
   scene.add(backLight);
 
+  const hemiLight = new THREE.HemisphereLight(0x000000, 0xffff00, 1);
+  scene.add(hemiLight);
+
   // postprocessing
   const postProcessing = new THREE.PostProcessing(renderer);
   const scenePass = pass(scene, camera);
   const scenePassColor = scenePass.getTextureNode();
 
-  // const dotScreenPass = dotScreen(scenePassColor);
-  // dotScreenPass.scale.value = 0.4;
+  const dotScreenPass = dotScreen(scenePassColor);
+  dotScreenPass.scale.value = 0.4;
 
   const sobelPass = sobel(scenePassColor);
 
-  const rgbShiftPass = rgbShift(sobelPass);
+  const rgbShiftPass = rgbShift(dotScreenPass);
   rgbShiftPass.amount.value = 0.005;
 
   const pixelation = pixelationPass(scene, camera);
@@ -162,28 +155,32 @@ function initScene(sceneData) {
   pixelation.normalEdgeStrength = 0.3;
   pixelation.depthEdgeStrength = 0.4;
 
-  // const afterImagePass = afterImage(scenePassColor);
-  // afterImagePass.damp.value = 0.96;
+  const afterImagePass = afterImage(sobelPass);
+  afterImagePass.damp.value = 0.96;
 
   const bloomPass = bloom(sobelPass);
-  // bloomPass.strength = 1.5;
-  // bloomPass.radius = 0.4;
-  // bloomPass.threshold = 0.0;
-
+  bloomPass.strength = 1.5;
+  bloomPass.radius = 0.4;
+  bloomPass.threshold = 0.0;
 
   const outlinePass = outline(scene, camera, { 
-    selectedObjects: [character, plane],
+    selectedObjects: [character],
    });
 
   postProcessing.outputNode = scenePass;
-  
+
   const clock = new THREE.Clock();
+  let nextTime = 2;
   function animate() {
     const delta = clock.getDelta();
-    character?.userData.update(delta); // todo calculate delta
+    character?.userData.update(delta);
     // renderer.render(scene, camera);
     postProcessing.render();
     controls.update();
+    if (clock.getElapsedTime() > nextTime) {
+      playRandomAnimationClip();
+      nextTime += 4;
+    }
   }
   renderer.setAnimationLoop(animate);
 
@@ -203,9 +200,9 @@ function initScene(sceneData) {
 
     const action = actions[index];
     if (action !== previousAction) {
-      previousAction?.fadeOut(1);
+      previousAction?.fadeOut(2);
       action.reset();
-      action.fadeIn(1);
+      action.fadeIn(2);
       action.play();
       previousAction = action;
     }
